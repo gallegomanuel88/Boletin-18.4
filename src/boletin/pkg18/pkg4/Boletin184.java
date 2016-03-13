d33e6aca9bba4b9741e98080ba3d266422f841ed
galle <galle@192.168.0.16>
2016-03-13 15:42:05 +0100

    - Fin

---
+++ src/boletin/pkg18/pkg4/Boletin184.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg18.pkg4;
+
+/**
+ *
+ * @author galle
+ */
+public class Boletin184 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         DNI mi_dni = new DNI();
+        mi_dni.pedirDNI();
+        mi_dni.calcularLetra();
+    }
+    
+}
